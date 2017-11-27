d7a608f8ef65c711d97db64d225ecf3a27663683
James A. Peltier <james_a_peltier@yahoo.ca>
2017-11-27 15:31:57 -0800

    triple the number of client pool connections
    hopefully this helps with client timeouts

--- manifests/params.pp
+++ manifests/params.pp
@@ -48,7 +48,7 @@ class puppet_stack::params {
   $foreman_db_adapter        = 'sqlite3'
   $foreman_db_host           = 'localhost'
   $foreman_db_name           = 'foreman'
-  $foreman_db_pool           = '25'
+  $foreman_db_pool           = '75'
   $foreman_db_timeout        = '5000'
   $foreman_db_user           = 'foreman'
   $foreman_vhost_server_name = $::fqdn
