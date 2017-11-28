0c8ef5e865e67368b9264ad5cbeae1d00e704839
James A. Peltier <james_a_peltier@yahoo.ca>
2017-11-27 21:15:25 -0800

    Update params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -49,7 +49,7 @@ class puppet_stack::params {
   $foreman_db_host           = 'localhost'
   $foreman_db_name           = 'foreman'
   $foreman_db_pool           = '75'
-  $foreman_db_timeout        = '5000'
+  $foreman_db_timeout        = '10000'
   $foreman_db_user           = 'foreman'
   $foreman_vhost_server_name = $::fqdn
   $foreman_url               = "https://${::fqdn}"
